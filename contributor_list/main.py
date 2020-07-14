b5b7f7301e694cf36da8d38ddbbc82f6408333c7
Caleb Denio <cjdenio44@gmail.com>
2020-07-14 19:27:30 -0400

    ✨ 🎨 Format and Git stuff

--- contributor_list/main.py
+++ contributor_list/main.py
@@ -8,7 +8,8 @@ r = requests.get(
     f"https://api.github.com/repos/{os.environ["GITHUB_REPOSITORY"]}/contributors"
 )
 
-file_path = pathlib.Path(__file__).parent.absolute().joinpath("default_template.md")
+file_path = pathlib.Path(__file__).parent.absolute().joinpath(
+    "default_template.md")
 
 with open(file_path) as template_file:
     template = Template(template_file.read())
@@ -21,8 +22,16 @@ with open("README.md") as _readme:
 if not "<!-- DO NOT REMOVE - contributor_list:start -->" in readme:
     print("Contributor list not found - creating it now! 🎉")
     with open("README.md", "a") as readme:
-        readme.write("\n<!-- DO NOT REMOVE - contributor_list:start -->\n" + rendered + "\n<!-- DO NOT REMOVE - contributor_list:end -->")
+        readme.write("\n<!-- DO NOT REMOVE - contributor_list:start -->\n" +
+                     rendered + "\n<!-- DO NOT REMOVE - contributor_list:end -->")
 else:
-    readme = re.sub('(?<=<!-- DO NOT REMOVE - contributor_list:start -->\n).+?(?=\n<!-- DO NOT REMOVE - contributor_list:end -->)', rendered, readme, flags=re.DOTALL)
+    readme = re.sub('(?<=<!-- DO NOT REMOVE - contributor_list:start -->\n).+?(?=\n<!-- DO NOT REMOVE - contributor_list:end -->)',
+                    rendered, readme, flags=re.DOTALL)
     with open("README.md", "w") as _readme:
         _readme.write(readme)
+
+os.system('git config --global user.email "action@github.com"')
+os.system('git config --global user.name "Publishing Bot"')
+os.system('git add .')
+os.system('git commit -m "Update contributors list"')
+os.system('git push')
